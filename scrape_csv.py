6e68897929a50d6b39d8d466dfadb83839066d9d
Catherine Zhang <czhang@hamilton.edu>
2026-03-02 15:30:45 -0500

    Struggling w/ Pandas Dependency + Transforms Search Query to JSON text file

--- scrape_csv.py
+++ scrape_csv.py
@@ -1,5 +1,7 @@
-import csv
-import json
+import pandas as pd
 
 csv_file = 'Jazz_Interviews - Jazz_Interviews.csv'
 
+# CITE: https://www.geeksforgeeks.org/pandas/reading-specific-columns-of-a-csv-file-using-pandas/
+df = pd.read_csv(csv_file, usecols=['subject_topical'])
+print(df)
