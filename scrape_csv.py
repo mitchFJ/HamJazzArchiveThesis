291195dad862e49f0e0dee2e509cc7781674235e
Catherine Zhang <czhang@hamilton.edu>
2026-03-05 23:39:40 -0500

    Creates Text File to store scraped label data
    Added code to create a text file which will store filter list.

--- scrape_csv.py
+++ scrape_csv.py
@@ -1,11 +1,16 @@
 import csv
 
 file_name = 'Jazz_Interviews - Jazz_Interviews.csv'
+new_file_name = 'test.txt'
 
-# CITE: https://docs.python.org/3/library/csv.html
-with open(file_name, newline='') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
+# CITE: https://www.geeksforgeeks.org/python/create-a-new-text-file-in-python/
+with open(new_file_name, 'w') as file:
+    # CITE: https://docs.python.org/3/library/csv.html
+    with open(file_name, newline='') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
 
-    for row in csv_reader:
-        labels = row['subject_topical']
-        print(labels)
+        for row in csv_reader:
+            labels = row['subject_topical']
+            file.write(labels + '\n')
+
+print("File '{new_file_name}' created successfully.")
