539d43bcb4aae162ac4779800bb8944b224376a8
Catherine Zhang <czhang@hamilton.edu>
2026-03-08 14:15:29 -0400

    Created Filter System
    Created the basic filter system.

--- Filter/filtersys.py
+++ Filter/filtersys.py
@@ -1,16 +1,45 @@
 # Filter System for Website
+import csv
 
-include_list = []
-exclude_list = []
+include_list = ["composers"]
+exclude_list = ["jazz"]
 
 doc_list = []
+file_name = 'Jazz_Interviews - Jazz_Interviews.csv'
 
-if len(include_list) > 1:
-    print("Has Include Filters")
-else:
-    print("Does not have Include Filters")
+with open(file_name, newline='') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
 
-if len(exclude_list) > 1:
+    if len(include_list) > 0:
+        print("Has Include Filters")
+
+        for row in csv_reader:
+            for label in include_list:
+                label = '"label":"' + label + '"'
+
+                if label in row['subject_topical']:
+                    doc_list.append(dict(name = row['label'], labels = row['subject_topical']))
+    else:
+        print("No Include Filters")
+
+        for row in csv_reader:
+            doc_list.append(dict(name = row['label'], labels = row['subject_topical']))
+
+# Exclude Filters not Working (Why is it no finding the Jazz label in all the docs?)
+# Registers it as a string
+if len(exclude_list) > 0:
     print("Has Exclude Filters")
+    i = 0
+
+    for row in doc_list:
+        for label in exclude_list:
+            label = '"label":"' + label + '"'
+            if label in row['labels']:
+                doc_list.pop(i)
+        i += 1
 else:
-    print("Does not have Exclude Filters")
+    print("No Exclusionary Filters")
+
+print(len(doc_list))
+# for row in doc_list:
+#     print(row['name'] + " " + row['labels'])
