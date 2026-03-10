aaabb5b7f68534328c4dc2ed4e78f61fb6559d74
Catherine Zhang <czhang@hamilton.edu>
2026-03-10 15:39:07 -0400

    Fixed Exclusion Filters
    Also made a function for checking lists

--- Filter/filtersys.py
+++ Filter/filtersys.py
@@ -1,45 +1,48 @@
 # Filter System for Website
 import csv
 
-include_list = ["composers"]
-exclude_list = ["jazz"]
+doc_name = 'label'
+labels_name = 'subject_topical'
 
-doc_list = []
-file_name = 'Jazz_Interviews - Jazz_Interviews.csv'
+include_list = []
+exclude_list = []
+
+include_docs = []
+exclude_docs = []
+file_name = 'Data/Jazz_Interviews - Jazz_Interviews.csv'
+
+def check_list(doc_list, new_doc_list, label_list, is_included):
+    for row in doc_list:
+        for label in label_list:
+            if label in row[labels_name]:
+                if is_included:
+                    new_doc_list.append(dict(label = row[doc_name], subject_topical = row[labels_name]))
+                break
+
+            if not is_included:
+                new_doc_list.append(dict(label = row[doc_name], subject_topical = row[labels_name]))
+    return
 
 with open(file_name, newline='') as csv_file:
     csv_reader = csv.DictReader(csv_file)
 
     if len(include_list) > 0:
-        print("Has Include Filters")
-
-        for row in csv_reader:
-            for label in include_list:
-                label = '"label":"' + label + '"'
-
-                if label in row['subject_topical']:
-                    doc_list.append(dict(name = row['label'], labels = row['subject_topical']))
+        # print("Has Include Filters")
+        check_list(csv_reader, include_docs, include_list, True)
     else:
-        print("No Include Filters")
-
+        # print("No Include Filters")
         for row in csv_reader:
-            doc_list.append(dict(name = row['label'], labels = row['subject_topical']))
+            include_docs.append(dict(label = row[doc_name], subject_topical = row[labels_name]))
 
-# Exclude Filters not Working (Why is it no finding the Jazz label in all the docs?)
-# Registers it as a string
+# print(len(include_docs))
+
 if len(exclude_list) > 0:
-    print("Has Exclude Filters")
-    i = 0
-
-    for row in doc_list:
-        for label in exclude_list:
-            label = '"label":"' + label + '"'
-            if label in row['labels']:
-                doc_list.pop(i)
-        i += 1
+    # print("Has Exclude Filters")
+    check_list(include_docs, exclude_docs, exclude_list, False)
 else:
-    print("No Exclusionary Filters")
+    # print("No Exclusionary Filters")
+    exclude_docs = include_docs
 
-print(len(doc_list))
-# for row in doc_list:
+# print(len(exclude_docs))
+# for row in relevant_docs:
 #     print(row['name'] + " " + row['labels'])
