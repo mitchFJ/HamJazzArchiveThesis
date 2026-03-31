1a39a4f4134f9119381469c254055e94b5bfad95
Catherine Zhang <czhang@hamilton.edu>
2026-03-31 19:39:02 -0400

    Converted each step of the filter system into commands
    Tried to search for a quicker solution, but the any() function seems to be the fastest way so far. Will put this efficiency concern for later.

--- Filter/filtersys.py
+++ Filter/filtersys.py
@@ -1,41 +1,61 @@
 # Filter System for Website
 import csv
 
+# To find docs
+file_path = 'Data/Jazz_Interviews - Jazz_Interviews.csv'
+
 doc_identifier = 'local_identifier'
 labels_name = 'subject_topical'
 
-include_list = []
-exclude_list = []
+# Filters out the document list
+def check_list(doc_list, label_list, is_included):
+    new_doc_list = []
 
-include_docs = []
-exclude_docs = []
-
-file_name = 'Data/Jazz_Interviews - Jazz_Interviews.csv'
-
-def check_list(doc_list, new_doc_list, label_list, is_included): # Precalculate all the permutation / Divide & Conquer
-    for row in doc_list:
-        for label in label_list:
-            if label in row[labels_name]:
-                if is_included:
-                    doc_name = "Data/Transcripts/output/" + row[doc_identifier][2:len(row[doc_identifier]) - 2] + ".pdf"
-                    new_doc_list.append(dict(label = doc_name, subject_topical = row[labels_name]))
-                break
-
+    # Checks through each doc to see if a label is in the doc_list
+    for doc in doc_list:
+        if any(label in doc['subject_topical'] for label in label_list):
+            if is_included:
+                new_doc_list.append(doc)
+        else:
             if not is_included:
-                new_doc_list.append(dict(label = row[doc_identifier], subject_topical = row[labels_name]))
-    return
+                new_doc_list.append(doc)
 
-with open(file_name, newline='') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
+    return new_doc_list
 
-    if len(include_list) > 0:
-        check_list(csv_reader, include_docs, include_list, True)
-    else:
+def filter_docs(doc_list, include_list, exclude_list):
+    new_doc_list = []
+
+    # Checks if both filter lists have no elements
+    if len(include_list) < 1 and len(exclude_list) < 1:
+        new_doc_list = doc_list
+        return new_doc_list
+
+    # Check if documents have include filters
+    if len(include_list) >= 1:
+        new_doc_list = check_list(doc_list, include_list, True)
+    
+    # Check if documents have exclude filters
+    if len(exclude_list) >= 1:
+        new_doc_list = check_list(doc_list, exclude_list, False)
+    
+    return new_doc_list
+
+# Gives the document path in the project
+def get_doc_path(doc_identifier):
+    doc_path = "Data/Transcripts/output/"
+    ext = ".pdf"
+
+    return doc_path + doc_identifier + ext
+
+# Gets the documents from the CSV
+def get_docs():
+    doc_list = []
+
+    with open(file_path, newline='') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+
         for row in csv_reader:
-            doc_name = "Data/Transcripts/output/" + row[doc_identifier][2:len(row[doc_identifier]) - 2] + ".pdf"
-            include_docs.append(dict(label = doc_name, subject_topical = row[labels_name]))
+            doc_path = get_doc_path(row[doc_identifier][2:len(row[doc_identifier]) - 2])
+            doc_list.append(dict(path = doc_path, subject_topical = row[labels_name]))
 
-if len(exclude_list) > 0:
-    check_list(include_docs, exclude_docs, exclude_list, False)
-else:
-    exclude_docs = include_docs
+    return doc_list
