60333ac4a9116b5197bff0fb2fc121e8c22a8be5
Catherine Zhang <czhang@hamilton.edu>
2026-03-31 19:40:24 -0400

    Added comments

--- Filter/filtersys.py
+++ Filter/filtersys.py
@@ -13,6 +13,7 @@ def check_list(doc_list, label_list, is_included):
 
     # Checks through each doc to see if a label is in the doc_list
     for doc in doc_list:
+        # CITE: https://www.geeksforgeeks.org/python/python-test-if-string-contains-element-from-list/
         if any(label in doc['subject_topical'] for label in label_list):
             if is_included:
                 new_doc_list.append(doc)
@@ -22,6 +23,7 @@ def check_list(doc_list, label_list, is_included):
 
     return new_doc_list
 
+# Checks to see if there are any filters active in the documents
 def filter_docs(doc_list, include_list, exclude_list):
     new_doc_list = []
 
