5f8e94e4ced13d35bda25c9ee073471973b13294
Catherine Zhang <czhang@hamilton.edu>
2026-04-07 14:40:22 -0400

    Implemented Combine CSV function

---
+++ Utilities/get_docs.py
@@ -0,0 +1,65 @@
+# Filter System for Website
+import csv
+import glob
+
+# To find docs
+csv_file_paths = ['Data/Transcript_List/Jazz_Interviews - Jazz_Interviews.csv', 'Data/Trasncript_List/Jazz_Interviews_Transcript_only - Jazz_Interviews_Transcript_only.csv']
+
+# Document Information
+uuid = 'node_uuid'
+doc_identifier = 'local_identifier'
+doc_name = 'label'
+labels_name = 'subject_topical'
+doc_type = 'genre'
+
+# Gives the document path in the project
+def get_doc_path(doc_identifier):
+    doc_path = 'Data/Transcripts/output/'
+    ext = '.pdf'
+
+    return doc_path + doc_identifier + ext
+
+# Gets the list of labels
+def get_label_list(doc):
+    label_str = doc[2:len(doc) - 2]
+    labels_list = label_str.split('},{')
+    return labels_list
+
+# Gets the documents from the CSV
+def get_docs_from_csv(csv_file_path):
+    doc_list = []
+
+    with open(csv_file_path, newline='') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        i = 0
+
+        for row in csv_reader:
+            doc_path = get_doc_path(row[doc_identifier][2:len(row[doc_identifier]) - 2])
+            label_list = get_label_list(row[labels_name])
+            doc_list.append(dict(index = i, id = row[uuid], name = row[doc_name], type = row[doc_type], path = doc_path, subject_topical = label_list))
+
+            i += 1
+
+    return doc_list
+
+# CITE: https://pytutorial.com/how-to-merge-multiple-csv-files-in-python-complete-guide/
+def combine_csv():
+    new_csv = 'Data/Jazz_Interviews_Doc_List.csv'
+    csv_files = glob.glob('Data/Transcript_List/*.csv')
+
+    # CITE: https://blog.finxter.com/5-best-ways-to-retrieve-the-first-row-from-a-csv-in-python/
+    with open(csv_files[0], 'r') as f:
+        headers = next(csv.reader(f))
+
+    with open(new_csv, 'w', newline='') as output:
+        writer = csv.writer(output)
+        writer.writerow(headers)
+        
+        for file in csv_files:
+            with open(file, 'r') as input:
+                reader = csv.reader(input)
+                next(reader)
+                for row in reader:
+                    writer.writerow(row)
+
+    return csv_files
