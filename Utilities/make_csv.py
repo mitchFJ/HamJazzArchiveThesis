eeadb4a18d8e955254c9509fd5b9fd25e0629366
mitchFJ <144164021+mitchFJ@users.noreply.github.com>
2026-05-02 17:48:10 -0400

    Added highlights in response

--- Utilities/make_csv.py
+++ Utilities/make_csv.py
@@ -2,31 +2,27 @@ from pypdf import PdfReader
 from pathlib import Path
 import re
 from sentence_transformers import SentenceTransformer
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
 from remove_linenum import redact_all
 import json
+import pandas as pd
+import numpy as np
+
 
 def save_text():
     model = SentenceTransformer('all-MiniLM-L6-v2')
-    cred = credentials.Certificate("Data/fillius-jazz-archive-search-firebase-adminsdk-fbsvc-cda02f015f.json")
-    firebase_admin.initialize_app(cred, {'databaseURL': 'https://fillius-jazz-archive-search-default-rtdb.firebaseio.com'})
-    ref = db.reference('/')
-    key = 0
     with open("Data/doc_list.json", "r") as file:
         pdfs = json.load(file)
+    all_text = []
+    all_pdf = []
+    all_pages = []
+    node_id = []
+    display_name = []
     for pdf in pdfs:
-        all_text = []
-        all_pdf = []
-        all_pages = []
-        node_id = []
-        display_name = []
         page = 1
         if ("Audiovisual Materials" in pdf["type"]):
             page = 2
         text = re.split("[A-Z]{2}:", exctract_text_from_pdf(pdf["path"]))
-        for i in range(1, len(text)-1):
+        for i in range(1, len(text) - 1):
             all_text.append(text[i-1].strip() + "\n" + text[i].strip() + "\n" + text[i+1].strip())
             all_pdf.append(pdf["path"])
             node_id.append(pdf["id"])
@@ -36,16 +32,35 @@ def save_text():
                 page += 1
             else:
                 all_pages.append(page)
+        num += 1
 
-        encoding = tokenize(all_text, model)
+    encoding = tokenize(all_text, model)
+    data = {"text": all_text,
+        "pdf": all_pdf,
+        "pagenum": all_pages,
+        "url": node_id,
+        "name": display_name
+    }
+    csv_file = Path("Data/extracted_text.csv")
+    df = pd.DataFrame(data)
+    df.to_csv(csv_file, index=False)
 
-        grouped_text = {}
-        for i in range(len(all_text)):
-            grouped_text[key + i] = [all_text[i], encoding[i].tolist(), str(all_pdf[i]),
-                                     all_pages[i], node_id[i], display_name[i]]
-        key += len(all_text)
+    encode_file = Path("Data/embeddings.npy")
+    np.save(encode_file, encoding)
+    current = all_pdf[0]
+    page_nums = []
+    make_json = {}
+    for i in range(len(all_pdf)):
+        if(current == all_pdf[i]):
+            page_nums.append(i)
+        else:
+            make_json[current] = page_nums.copy()
+            current = all_pdf[i]
+            page_nums = [i]
+    make_json[current] = page_nums.copy()
+    with open("Data/data_line_num.json", "w") as f:
+        json.dump(make_json, f, indent=4)
 
-        ref.update(grouped_text)
 
 def tokenize(text, model):
     encoding = model.encode(text)
@@ -60,5 +75,4 @@ def exctract_text_from_pdf(path):
     return full_text
 
 if __name__ == "__main__":
-    #redact_all()
     save_text()
