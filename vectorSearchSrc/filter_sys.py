807c4360c1ba76124fda5f0c6ff05e0633b6dec4
Catherine Zhang <czhang@hamilton.edu>
2026-05-20 14:49:36 -0400

    Update Filter System and Filtering Label List
    Transferred code of the systems and how they work to the vector search system folder.

--- vectorSearchSrc/filter_sys.py
+++ vectorSearchSrc/filter_sys.py
@@ -1,65 +1,48 @@
 import filtering_label_list as fll
 import json
 
+# Gets the information from doc list JSON file
 def get_doc_dict(input_file_path):
     with open(input_file_path, mode='r') as f:
         data = json.load(f)
 
     return data
 
-def get_values(dict_list):
-    value_list = []
-
-    for value in dict_list.values():
-        value_list.append(dict(value))
-
-    return value_list
-
-# Filters out the document list
+# Filters in documents that have the labels in the given label list
 def check_include(new_doc_list, label_list, doc_list, label_dict):
-    # Checks through each doc to see if a label is in the doc_list
+    # Checks through each doc to see if a label is in the doc list
     for label in label_list:
         for doc in label_dict[label]:
             if doc_list[doc] not in new_doc_list:
                 new_doc_list.append(doc_list[doc])
-
     return
 
-# Filters out the document list
+# Filters out documents that have the labels in the given label list
 def check_exclude(new_doc_list, label_list, doc_list, label_dict):
     # Checks through each doc to see if a label is in the doc_list
     for label in label_list:
         for doc in label_dict[label]:
             if doc_list[doc] in new_doc_list:
                 new_doc_list.remove(doc_list[doc])
-
     return
 
 # Checks to see if there are any filters active in the documents
-def filter_docs(include_list = [], exclude_list = []): # Are the lists files or lists?
+def filter_docs(include_list = [], exclude_list = []):
+    # Variables for the program to function
     new_doc_list = []
-    doc_list = get_doc_dict('Data/doc_list.json')
+    doc_list = get_doc_dict('../Data/doc_list.json')
     label_dict = fll.link_label_to_docs(doc_list)
-    create_txt(label_dict, "Data/label_dict.json")
 
     # Checks if both filter lists have no elements
-    # Check if there are include filters
+    ## Check if there are include filters
     if len(include_list) > 0:
         check_include(new_doc_list, include_list, doc_list, label_dict)
-    
-    # Check if documents have exclude filters
+    ## Check if documents have exclude filters
     if len(exclude_list) > 0:
         check_exclude(new_doc_list, exclude_list, doc_list, label_dict)
 
     # Checks if the document list is empty
     if len(new_doc_list) < 1:
-        new_doc_list = get_values(doc_list)
+        new_doc_list = list(doc_list.values())
     
     return new_doc_list
-
-def create_txt(doc_list, file_name):
-
-    with open(file_name, 'w') as file:
-        json.dump(doc_list, file, indent=4)
-    
-    return
--- vectorSearchSrc/filtering_label_list.py
+++ vectorSearchSrc/filtering_label_list.py
@@ -1,5 +1,18 @@
-label_list_file_path = 'Data/scraped_labels.txt'
+# Creates a label dictionary for the document paths to be stored
+def create_label_dict(label_list_txt = '../Data/scraped_labels.txt'):
+    label_dict = {}
 
+    with open(label_list_txt) as f:
+        labels_list = f.readlines()
+    
+    for label in labels_list:
+        if '\n' in label:
+            label = label[:len(label) - 1]
+        label_dict.update({label: []})
+
+    return label_dict
+
+# Sets the doc path to the doc's corresponding labels in the label dictionary for ease of searching
 def set_doc_to_labels(doc, doc_list, label_dict):
     for key in doc_list[doc]['subject_topical']:
         if len(label_dict[key]) < 1:
@@ -10,19 +23,7 @@ def set_doc_to_labels(doc, doc_list, label_dict):
         label_dict.update({key: new_doc_list})
     return
 
-def create_label_dict():
-    label_dict = {}
-
-    with open(label_list_file_path) as f:
-        labels_list = f.readlines()
-    
-    for label in labels_list:
-        if '\n' in label:
-            label = label[:len(label) - 1]
-        label_dict.update({label: []})
-
-    return label_dict
-
+# Links each doc to each entry in the label dictionary
 def link_label_to_docs(doc_list, label_dict = create_label_dict()):
     for doc in doc_list:
         set_doc_to_labels(doc, doc_list, label_dict)
